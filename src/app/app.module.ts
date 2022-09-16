72a7358d5a51640787e090f94db0d985d06c380a
Ebo Arcadia <yiboli.coding@gmail.com>
2022-09-16 09:22:40 -0400

    add booking component, route, global css, render app component successfully

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+import { BookingComponent } from './components/booking/booking.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookingComponent,
   ],
   imports: [
     BrowserModule,

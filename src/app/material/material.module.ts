d43e3ed78a048d199da97036c01611de6f40b532
Ebo Arcadia <yiboli.coding@gmail.com>
2022-09-19 09:30:49 -0400

    add icons using google font

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 const MaterialComponents = [
   MatButtonModule,
+  MatIconModule,
 ]
 
 @NgModule({

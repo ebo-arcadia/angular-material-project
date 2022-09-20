75c3c9172decf9241aa76f2a9dc54ec5d2f9f087
Ebo Arcadia <yiboli.coding@gmail.com>
2022-09-20 09:27:16 -0400

    add input box, implementing different effect using angular material tag properties

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,17 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 const MaterialComponents = [
   MatButtonModule,
   MatIconModule,
+  MatInputModule,
+  MatFormFieldModule
 ]
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    MaterialComponents,
+    MaterialComponents
   ],
   exports: [MaterialComponents]
 })

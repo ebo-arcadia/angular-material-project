4773145b0afb01daf31f9df16639a63640321618
Ebo Arcadia <yiboli.coding@gmail.com>
2022-09-23 09:20:37 -0400

    add reactive form

--- src/app/components/booking/booking.component.ts
+++ src/app/components/booking/booking.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CountriesService } from 'src/app/services/countries.service';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-booking',
@@ -9,8 +10,15 @@ import { CountriesService } from 'src/app/services/countries.service';
 export class BookingComponent implements OnInit {
   countries: any;
   error: any;
+  formGroup: FormGroup
 
-  constructor(private _countriesService: CountriesService) { }
+  constructor(private _countriesService: CountriesService) {
+    this.formGroup = new FormGroup({
+      place: new FormControl(null),
+      guestName: new FormControl(null),
+      country: new FormControl(null)
+    });
+  }
 
   ngOnInit(): void {
     this._countriesService.getCountries().subscribe({

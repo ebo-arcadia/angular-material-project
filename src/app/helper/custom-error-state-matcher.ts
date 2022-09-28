0fd2b290607ac8cad0ac78fd13ec610e37dbdda2
Ebo Arcadia <yiboli.coding@gmail.com>
2022-09-28 19:28:06 -0400

    fix error state match possible null value for control and form, add property dirty

--- src/app/helper/custom-error-state-matcher.ts
+++ src/app/helper/custom-error-state-matcher.ts
@@ -1,8 +1,9 @@
 import { ErrorStateMatcher } from "@angular/material/core";
-import { FormControl, FormGroupDirective } from "@angular/forms";
+import { FormControl, FormGroupDirective, NgForm } from "@angular/forms";
 
 export class CustomErrorStateMatcher implements ErrorStateMatcher {
-    isErrorState(control: FormControl, form: FormGroupDirective): boolean {
-        return control.invalid && control.dirty
+    isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+        const isSubmitted = form && form.submitted
+        return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
     }
 }

26f7ba182c8fc2ac513c9bf8b09114708c959931
Ebo Arcadia <yiboli.coding@gmail.com>
2022-10-27 19:37:50 -0400

    refactor mat table using data source

--- src/app/components/booking-list/booking-list.component.ts
+++ src/app/components/booking-list/booking-list.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BookingService } from 'src/app/services/bookings.service';
 import { Booking } from 'src/app/model/Booking';
-import { Observable } from 'rxjs';
 import { MatTableDataSource } from "@angular/material/table";
 
 @Component({
@@ -11,15 +10,16 @@ import { MatTableDataSource } from "@angular/material/table";
 })
 export class BookingListComponent implements OnInit {
 
-  bookings: Booking[] = [];
-  dataSource = new MatTableDataSource<Booking>();
-  displayedTableColumns: string[] = ['guestId', 'guestName'];
+  bookings: MatTableDataSource<Booking> = new MatTableDataSource<Booking>([]);;
+  displayedTableColumns: string[] = ['guestId', 'guestName', 'location', 'date'];
 
   constructor(private readonly _bookingsService: BookingService) { }
 
 
   ngOnInit(): void{
-    this._bookingsService.getBookings().subscribe((response) => { this.dataSource.data = response});
+    this._bookingsService.getBookings().subscribe((response) => { 
+      this.bookings = new MatTableDataSource<Booking>(response)
+    });
   }
 
 }

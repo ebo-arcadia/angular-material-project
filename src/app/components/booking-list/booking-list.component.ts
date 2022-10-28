469c65e64f1b584ac4ee2f282a409d104cb55809
Ebo Arcadia <yiboli.coding@gmail.com>
2022-10-28 08:48:00 -0400

    add paginator

--- src/app/components/booking-list/booking-list.component.ts
+++ src/app/components/booking-list/booking-list.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild} from '@angular/core';
 import { BookingService } from 'src/app/services/bookings.service';
 import { Booking } from 'src/app/model/Booking';
 import { MatTableDataSource } from "@angular/material/table";
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-booking-list',
@@ -13,12 +14,15 @@ export class BookingListComponent implements OnInit {
   bookings: MatTableDataSource<Booking> = new MatTableDataSource<Booking>([]);;
   displayedTableColumns: string[] = ['guestId', 'guestName', 'location', 'date'];
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
   constructor(private readonly _bookingsService: BookingService) { }
 
 
   ngOnInit(): void{
     this._bookingsService.getBookings().subscribe((response) => { 
-      this.bookings = new MatTableDataSource<Booking>(response)
+      this.bookings = new MatTableDataSource<Booking>(response);
+      this.bookings.paginator = this.paginator;
     });
   }
 
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -14,6 +14,7 @@ import { MatButtonToggleModule } from '@angular/material/button-toggle'
 import { MatChipsModule } from '@angular/material/chips';
 import { MatDialogModule } from "@angular/material/dialog";
 import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 const MaterialComponents = [
   MatButtonModule,
@@ -29,7 +30,8 @@ const MaterialComponents = [
   MatButtonToggleModule,
   MatChipsModule,
   MatDialogModule,
-  MatTableModule
+  MatTableModule,
+  MatPaginatorModule
 ];
 
 @NgModule({
